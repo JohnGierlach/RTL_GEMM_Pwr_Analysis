56c04f6c8eba4b8dbe7279ebdc7b04b6e9157bd8
John Gierlach <jgierlach17@gmail.com>
2024-10-29 11:02:52 -0400

    Added IO ports for gemm_top module | TODO: Plan our paralell design

--- rtl/gemm_top.v
+++ rtl/gemm_top.v
@@ -1,8 +1,16 @@
 `timescale 1ps/1ps
 
-module gemm_top(
-    input iclk, irst
-    output
+module gemm_top
+#(DATA_WIDTH = 32,
+  MATRIX_WIDTH = 4,
+  MATRIX_HEIGHT = 4,
+  MATRIX_ADJUST = 4)(
+    input iclk, irst,
+    input[DATA_WIDTH-1:0] alpha, beta,
+    input[DATA_WIDTH-1:0] a_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    input[DATA_WIDTH-1:0] b_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    input[DATA_WIDTH-1:0] c_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    output[DATA_WIDTH-1:0] result_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1]
 );
 
 
