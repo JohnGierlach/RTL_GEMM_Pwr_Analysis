1f4d0627526d05375a3ab23037bb25f3e1ff27ae
John Gierlach <jgierlach17@gmail.com>
2024-10-31 15:06:20 -0400

    Rewrote the design module iterate array indecies within the sequential block | TODO: Still getting wrong values

--- rtl/gemm_tb.sv
+++ rtl/gemm_tb.sv
@@ -2,23 +2,27 @@ module gemm_tb();
     
     parameter DATA_WIDTH = 64, MATRIX_WIDTH = 4, MATRIX_HEIGHT = 4, MATRIX_ADJUST = 4;
     
-    reg iclk, irst;
-    real a_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
-    real b_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
-    real c_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
-    reg[DATA_WIDTH-1:0] alpha, beta;
+    reg iclk, irst, istart;
+    logic signed [DATA_WIDTH-1:0] ia_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    logic signed [DATA_WIDTH-1:0] ib_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    logic signed [DATA_WIDTH-1:0] ic_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    reg[DATA_WIDTH-1:0] ialpha, ibeta;
 
-    wire[DATA_WIDTH-1:0] result_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    wire[DATA_WIDTH-1:0] oresult_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    wire obusy, odone;
 
 
     gemm_top UUT(.iclk(iclk),
                  .irst(irst),
-                 .alpha(alpha),
-                 .beta(beta),
-                 .a_matrix(a_matrix),
-                 .b_matrix(b_matrix),
-                 .c_matrix(c_matrix),
-                 .result_matrix(result_matrix));
+                 .istart(istart),
+                 .ialpha(ialpha),
+                 .ibeta(ibeta),
+                 .ia_matrix(ia_matrix),
+                 .ib_matrix(ib_matrix),
+                 .ic_matrix(ic_matrix),
+                 .oresult_matrix(oresult_matrix),
+                 .obusy(obusy),
+                 .odone(odone));
     
     real A_tmp, B_tmp, C_tmp;
     integer i, j, A_file, B_file, C_file;
@@ -34,20 +38,26 @@ module gemm_tb();
                 $fscanf(B_file, "%f", B_tmp);
                 $fscanf(C_file, "%f", C_tmp);
                 
-                a_matrix[i][j] = A_tmp;
-                b_matrix[i][j] = B_tmp;
-                c_matrix[i][j] = C_tmp;
+                ia_matrix[i][j] = A_tmp;
+                ib_matrix[i][j] = B_tmp;
+                ic_matrix[i][j] = C_tmp;
                 
             end
         end
 
         irst = 1;
         iclk = 1;
-        alpha = 1;
-        beta = 1;
+        ialpha = 1;
+        ibeta = 1;
+        istart = 0;
         #10
+        istart = 1;
         irst = 0;
         #10;
+        if(odone)begin
+            istart = 0;
+            $finish;
+        end
     end
 
     always #10 iclk = ~iclk;
--- rtl/gemm_top.sv
+++ rtl/gemm_top.sv
@@ -5,41 +5,83 @@ module gemm_top
   MATRIX_WIDTH = 4,
   MATRIX_HEIGHT = 4,
   MATRIX_ADJUST = 4)(
-    input iclk, irst,
-    input[DATA_WIDTH-1:0] alpha, beta,
-    input[DATA_WIDTH-1:0] a_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
-    input[DATA_WIDTH-1:0] b_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
-    input[DATA_WIDTH-1:0] c_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
-    output reg[DATA_WIDTH-1:0] result_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1]
+    input iclk, irst, istart,
+    input[DATA_WIDTH-1:0] ialpha, ibeta,
+    input logic signed [DATA_WIDTH-1:0] ia_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    input logic signed [DATA_WIDTH-1:0] ib_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    input logic signed [DATA_WIDTH-1:0] ic_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    output reg[DATA_WIDTH-1:0] oresult_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    output obusy, odone
 );
-
-
-    //reg[DATA_WIDTH-1:0] tmp_matrix[0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    
+    // State Transition parameters
+    localparam IDLE = 0, COMPUTE = 1, DONE = 2;
+    
+    reg[DATA_WIDTH-1:0] tmp_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
+    reg[1:0] state, next_state;
     reg[DATA_WIDTH-1:0] sum;
-    integer i, j, k;
-
+    int i, j, k;
+    
+    always@(*)begin
+        case(state)
+            IDLE: next_state = (istart) ? COMPUTE : IDLE;
+            COMPUTE: next_state = (i == MATRIX_HEIGHT) && 
+                                  (j == MATRIX_WIDTH)  && 
+                                  (k == MATRIX_ADJUST) ? COMPUTE : IDLE;
+            DONE: next_state = IDLE;
+        endcase
+    end
+    
+    
+    always@(posedge iclk)begin
+        if(irst)
+            state <= IDLE;
+        
+        else
+            state <= next_state;
+    end
+    
     always@(posedge iclk)begin
         if(irst)begin
-            sum <= 32'h0000;
+            sum <= 64'h0000;
+            i <= 0;
+            j <= 0;
+            j <= 0;
             for(i = 0; i < MATRIX_HEIGHT; i = i + 1) begin
                 for(j = 0; j < MATRIX_WIDTH; j = j + 1)
-                    
-                    result_matrix[i][j] <= c_matrix[i][j];
+                    tmp_matrix[i][j] <= ic_matrix[i][j];
             end
         end
-
-        else begin
-            for(i = 0; i < MATRIX_HEIGHT; i = i + 1) begin
-                for(j = 0; j < MATRIX_WIDTH; j = j + 1)begin
-                    sum <= 32'h0000;
-                    for(k = 0; k < MATRIX_ADJUST; k = k + 1)begin
-                        sum <= sum + (a_matrix[i][k] + b_matrix[k][j]);
-                    end
-                    result_matrix[i][j] <= (alpha * sum) + (beta * result_matrix[i][j]);
+        
+        // Compute the matrix
+        else if(state == COMPUTE)begin
+            sum <= sum + (ia_matrix[i][k] * ib_matrix[k][j]);
+            tmp_matrix[i][j] <= (ialpha * sum) + (ibeta * tmp_matrix[i][j]);
+            // Update looking variables
+            if(k < MATRIX_ADJUST - 1)
+                k <= k + 1;
+            
+            else begin
+                k <= 0;
+                if(j < MATRIX_WIDTH - 1)
+                    j <= j + 1;
+                else begin
+                    j <= 0;
+                    if(i < MATRIX_HEIGHT - 1)
+                        i <= i + 1;
+                    else
+                        i <= 0;
                 end
             end
         end
-    end
 
+        else if(state == DONE)begin
+            foreach (oresult_matrix[i,j]) oresult_matrix[i][j] <= tmp_matrix[i][j];
+        end 
+    end
+    
+    
+    assign obusy = state == COMPUTE;
+    assign odone = state == DONE;
 
 endmodule
