363a24ececb408e6b8936478f4c2900f6c9f7358
John Gierlach <jgierlach17@gmail.com>
2024-12-12 13:44:44 -0500

    Added structure for parallelizing the sum of mat mult

--- rtl/gemm_top.sv
+++ rtl/gemm_top.sv
@@ -8,8 +8,8 @@ module gemm_top
     input iclk, irst, istart,
     input[DATA_WIDTH-1:0] ialpha, ibeta,
     input logic signed [DATA_WIDTH-1:0] ia_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
-    input logic signed [DATA_WIDTH-1:0] ib_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
-    input logic signed [DATA_WIDTH-1:0] ic_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
+    input logic signed [DATA_WIDTH-1:0] ib_matrix [0:MATRIX_ADJUST-1][0:MATRIX_WIDTH-1],
+    input logic signed [DATA_WIDTH-1:0] ic_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_ADJUST-1],
     output reg[DATA_WIDTH-1:0] oresult_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1],
     output obusy, odone
 );
@@ -20,14 +20,32 @@ module gemm_top
     reg[DATA_WIDTH-1:0] tmp_matrix [0:MATRIX_HEIGHT-1][0:MATRIX_WIDTH-1];
     reg[1:0] state, next_state;
     reg[DATA_WIDTH-1:0] sum;
-    int i, j, k;
+    int i, j;
     
+
+    // Generate the matrix sum module in parallel
+    genvar gen_k;
+    generate
+        for(gen_k = 0; gen_k < MATRIX_WIDTH; gen_k = gen_k + 1)begin:gendgenerate
+            mat_sum #(
+                .DATA_WIDTH(DATA_WIDTH)
+            ) mat_sum_inst (
+                .iclk(iclk),
+                .irst(irst),
+                .icurr_sum(sum),
+                .ia_matrix_tile(ia_matrix[i][gen_k]),
+                .ib_matrix_tile(ib_matrix[gen_k][j]),
+                .osum(sum)
+            );
+        end
+    endgenerate
+
+
     always@(*)begin
         case(state)
             IDLE: next_state = (istart) ? COMPUTE : IDLE;
             COMPUTE: next_state = (i == MATRIX_HEIGHT-1) && 
-                                  (j == MATRIX_WIDTH-1)  && 
-                                  (k == MATRIX_ADJUST) ? DONE : COMPUTE;
+                                  (j == MATRIX_WIDTH-1)  && ? DONE : COMPUTE;
             DONE: next_state = IDLE;
         endcase
     end
@@ -46,7 +64,6 @@ module gemm_top
             sum <= 64'h0000;
             i <= 0;
             j <= 0;
-            k <= 0;
             for(i = 0; i < MATRIX_HEIGHT; i = i + 1) begin
                 for(j = 0; j < MATRIX_WIDTH; j = j + 1)
                     tmp_matrix[i][j] <= ic_matrix[i][j];
@@ -56,14 +73,7 @@ module gemm_top
         // Compute the matrix
         else if(state == COMPUTE)begin
             
-            // Update looking variables
-            if(k < MATRIX_ADJUST)begin
-                sum <= sum + (ia_matrix[i][k] * ib_matrix[k][j]);
-                k <= k + 1;
-            end
-
             else begin
-                k <= 0;
                 sum <= 0;
                 
                 if(j < MATRIX_WIDTH)begin
---
+++ rtl/mat_sum.sv
@@ -0,0 +1,26 @@
+module N_length_gemm
+#(DATA_WIDTH = 64,
+  MATRIX_WIDTH = 4,
+  MATRIX_HEIGHT = 4,
+  MATRIX_ADJUST = 4)(
+    input iclk, irst,
+    input reg[DATA_WIDTH-1:0] icurr_sum;
+    input logic signed [DATA_WIDTH-1:0] ia_matrix_tile,
+    input logic signed [DATA_WIDTH-1:0] ib_matrix_tile,
+    output reg[DATA_WIDTH-1:0] osum
+);
+
+
+reg[DATA_WIDTH-1:0] tmp_sum;
+
+
+always@(posedge iclk)begin
+  if(irst)
+    tmp_sum <= 64'h0000;
+  else
+    tmp_sum <= icurr_sum + (ia_matrix_tile * ib_matrix_tile);
+end
+
+assign osum = tmp_sum;
+
+endmodule
